d2c36998913a888d69d9c7eee92241bf4c348360
André Oliveira <andrelitoscria@gmail.com>
2024-04-09 23:52:25 -0300

    .
    .

--- batalha-naval/script.js
+++ batalha-naval/script.js
@@ -1,5 +1,18 @@
 const grid = document.querySelector('.grid');
-//CRIANDO O CARD PELO O JS
+
+//array para os cards
+const icons = [
+    'Ship-1',
+    'Ship-2',
+    'Ship-3',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Bomb',
+]
 //essa funçao a tem como parametro a tag e a class para economizar cod
 
 const createElement = (tag, classe) => {
@@ -8,7 +21,17 @@ const createElement = (tag, classe) => {
     return element;
 }
 
-const createCaard = () => {
+const revealCard = ({ target }) => {
+    if (target.parentNode.className.includes('reveal-card')){
+        return;
+    }  else{
+    target.parentNode.classList.add('reveal-card');
+    }
+}
+
+//CRIANDO O CARD PELO O JS
+
+const createCard = (icon) => {
     //criando a div card, frente e verso
     //adicionando class
     
@@ -16,13 +39,31 @@ const createCaard = () => {
     const frente = createElement('div', 'face frente');
     const verso = createElement('div', 'face verso');
     
+    frente.style.backgroundImage = `url('img/${icon}.png')`;
+    
     //colocando a div frente e a div verso dentro do card
     
     card.appendChild(frente);
     card.appendChild(verso);
 
+    card.addEventListener('click', revealCard);
+
     return card;
 
 }
 
-createCaard();
+const loadGame = () => {
+
+    const multicons = [ ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons]
+
+    const randomArray = multicons.sort( () => Math.random() - 0.5);
+
+    randomArray.forEach((icon) => {
+
+        const card = createCard(icon);
+        grid.appendChild(card);
+
+    });
+
+}
+loadGame();
