a2e34e4ae38b3bd11c286b2227df8bd0ce5a5058
André Oliveira <andrelitoscria@gmail.com>
2024-04-10 20:49:22 -0300

    .
    .

--- batalha-naval/script.js
+++ batalha-naval/script.js
@@ -11,6 +11,21 @@ const icons = [
     'Wave',
     'Wave',
     'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
+    'Wave',
     'Bomb',
 ]
 //essa funçao a tem como parametro a tag e a class para economizar cod
@@ -21,13 +36,52 @@ const createElement = (tag, classe) => {
     return element;
 }
 
+
+
+//adicionando a class para a animação e de quebra pegando src da imagem para o sistema de pontos 
+
 const revealCard = ({ target }) => {
+    
     if (target.parentNode.className.includes('reveal-card')){
         return;
     }  else{
     target.parentNode.classList.add('reveal-card');
     }
 }
+const score = () => {
+    let targetcard = document.querySelectorAll('.reveal-card img');
+
+    targetcard.forEach((card) => {
+        let srcCard = card.src;
+    })
+    
+    let score = 0;
+
+    let barco1 = 'Ship-1.png';
+    let barco2 = 'Ship-2.png';
+    let barco3 = 'Ship-3.png';
+    let Bomb = 'Bomb.png';
+    let Wave = 'Wave.png';
+    
+    switch (srcCard){
+        case Wave:
+            score =+ 0;
+        break;
+        case Bomb:
+            score = 0;
+        case barco1:
+            score =+ 5;
+        break;
+        case barco2:
+            score =+ 10;
+        break;
+        case barco3:
+            score =+ 15;
+        break;
+    }
+    console.log(soma);
+}
+
 
 //CRIANDO O CARD PELO O JS
 
@@ -54,7 +108,7 @@ const createCard = (icon) => {
 
 const loadGame = () => {
 
-    const multicons = [ ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons, ... icons]
+    const multicons = [ ... icons, ... icons, ... icons, ... icons]
 
     const randomArray = multicons.sort( () => Math.random() - 0.5);
 
